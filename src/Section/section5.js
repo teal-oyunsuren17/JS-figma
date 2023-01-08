a89eac1ae6098d6ab736c5f3af0f622b36ea70b4
teal-oyunsuren17 <teal.2022.oyunsuren.purevsuren@gmail.com>
2023-01-09 01:42:32 +0800

    add 4

--- src/Section/section5.js
+++ src/Section/section5.js
@@ -1,14 +1,15 @@
 export default function Section5() {
   return (
-    <div className="d-flex flex-row">
-      <div>
-        <img src="./images/speaker.png" alt="" />
-        <div>
-          <h4>nbb</h4>
-          <p>vv</p>
-        </div>
-      </div>
-      <div></div>
-    </div>
+    // <div className="d-flex flex-row">
+    //   <div>
+    //     <img src="./images/speaker.png" alt="" />
+    //     <div>
+    //       <h4>nbb</h4>
+    //       <p>vv</p>
+    //     </div>
+    //   </div>
+    //   <div></div>
+    // </div>
+    <div></div>
   );
 }
