fa8aa9c4f68208fc3d60d0186ec88b98f14e77a8
teal-oyunsuren17 <teal.2022.oyunsuren.purevsuren@gmail.com>
2023-01-12 17:48:40 +0800

    add at class

--- src/Section/s3TopProducts.js
+++ src/Section/s3TopProducts.js
@@ -2,22 +2,22 @@ import { AiOutlineHeart } from "react-icons/ai";
 import { AiOutlineShoppingCart } from "react-icons/ai";
 import StarBtn from "./starBtn";
 
-export default function S3TopProducts() {
+export default function S3Tops3Products() {
   return (
-    <div>
+    <div className="s3Main">
       <div className="s3header d-flex flex-row justify-content-between ">
-        <h2>Popular products</h2>
+        <h2>Popular s3Products</h2>
         <div className="s3HeaderBtns d-flex flex-row">
-          <button className="topProList">Cameras</button>
-          <button className="topProList">Laptops</button>
-          <button className="topProList">Tablets</button>
-          <button className="topProList">Mouse</button>
+          <button className="s3TopProList">Cameras</button>
+          <button className="s3TopProList">Laptops</button>
+          <button className="s3TopProList">Tablets</button>
+          <button className="s3TopProList">Mouse</button>
         </div>
       </div>
 
-      <div className="d-flex flex-row flex-wrap justify-content-around">
-        <div className="product">
-          <img src="./images/cameraFace.png" alt="" />
+      <div className="d-flex flex-row flex-wrap justify-content-between">
+        <div className="s3Product">
+          <img className="s3ProductImg" src="./images/cameraFace.png" alt="" />
           <button className="heartBtn">
             <AiOutlineHeart />
           </button>
@@ -29,8 +29,23 @@ export default function S3TopProducts() {
           </button>
         </div>
 
-        <div className="product">
-          <img src="./images/earphoneBig.png" alt="" />
+        <div className="s3Product">
+          <img className="s3ProductImg" src="./images/earphoneBig.png" alt="" />
+          <button className="heartBtn">
+            <AiOutlineHeart />
+          </button>
+          <div className="d-flex flex-column">
+            <h4>Camera</h4>
+            <p>$11,70</p>
+            <StarBtn />
+            <button className="shopBtn">
+              <AiOutlineShoppingCart />
+            </button>
+          </div>
+        </div>
+
+        <div className="s3Product">
+          <img className="s3ProductImg" src="./images/earphoneBig.png" alt="" />
           <button className="heartBtn">
             <AiOutlineHeart />
           </button>
@@ -42,8 +57,8 @@ export default function S3TopProducts() {
           </button>
         </div>
 
-        <div className="product">
-          <img src="./images/earphoneBig.png" alt="" />
+        <div className="s3Product">
+          <img className="s3ProductImg" src="./images/game.png" alt="" />
           <button className="heartBtn">
             <AiOutlineHeart />
           </button>
@@ -55,8 +70,8 @@ export default function S3TopProducts() {
           </button>
         </div>
 
-        <div className="product">
-          <img src="./images/game.png" alt="" />
+        <div className="s3Product">
+          <img className="s3ProductImg" src="./images/game.png" alt="" />
           <button className="heartBtn">
             <AiOutlineHeart />
           </button>
@@ -68,8 +83,8 @@ export default function S3TopProducts() {
           </button>
         </div>
 
-        <div className="product">
-          <img src="./images/game.png" alt="" />
+        <div className="s3Product">
+          <img className="s3ProductImg" src="./images/desktop.png" alt="" />
           <button className="heartBtn">
             <AiOutlineHeart />
           </button>
@@ -81,8 +96,8 @@ export default function S3TopProducts() {
           </button>
         </div>
 
-        <div className="product">
-          <img src="./images/desktop.png" alt="" />
+        <div className="s3Product">
+          <img className="s3ProductImg" src="./images/game.png" alt="" />
           <button className="heartBtn">
             <AiOutlineHeart />
           </button>
@@ -94,21 +109,8 @@ export default function S3TopProducts() {
           </button>
         </div>
 
-        <div className="product">
-          <img src="./images/game.png" alt="" />
-          <button className="heartBtn">
-            <AiOutlineHeart />
-          </button>
-          <h4>Camera</h4>
-          <p>$11,70</p>
-          <StarBtn />
-          <button className="shopBtn">
-            <AiOutlineShoppingCart />
-          </button>
-        </div>
-
-        <div className="product">
-          <img src="./images/desktop.png" alt="" />
+        <div className="s3Product">
+          <img className="s3ProductImg" src="./images/desktop.png" alt="" />
           <button className="heartBtn">
             <AiOutlineHeart />
           </button>
